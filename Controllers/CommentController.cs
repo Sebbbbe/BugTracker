555d2804b23fac2c42ced448d8710a72d34f5e73
Sebbbbe <sebastian.tiger901@gmail.com>
2021-04-15 21:16:21 +0200

    added comment controller

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,60 @@
+﻿using BugTrackerNew.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bugTrackerNew.Controllers
+{
+    [ApiController]
+    [Route("Comment")]
+    public class CommentController : ControllerBase
+    {
+
+        private BugTrackerDBContext _bugTrackerDBContext;
+        public CommentController(BugTrackerDBContext bugTrackerDBContext)
+        {
+            _bugTrackerDBContext = bugTrackerDBContext;
+        }
+
+        [HttpGet]
+        public List<Comment> GetComment()
+        {
+            var comment = _bugTrackerDBContext.Comments.ToList();
+            return comment;
+
+        }
+
+        [HttpPost]
+        public void UpdateComment(Comment comment )
+        {
+          
+            _bugTrackerDBContext.Comments.Add(comment);  
+            _bugTrackerDBContext.SaveChanges();
+
+        }
+
+        [HttpDelete ("{id:Guid}")] 
+
+        public void DeleteComment(Guid id )
+        {
+           var DeleteComment =  _bugTrackerDBContext.Comments.Find(id); // går till dbset comments som sedan kollar i comment efter id
+            _bugTrackerDBContext.Comments.Remove(DeleteComment); // deletar comment med id som sätts in
+            _bugTrackerDBContext.SaveChanges();
+
+        }
+
+        [HttpPut ("{id:Guid}")]
+
+        public void UpdateComment(Guid id , Comment comment)
+        {
+        var UpdateComment = _bugTrackerDBContext.Comments.Find(id);
+
+            UpdateComment.Comment_text = comment.Comment_text;
+            _bugTrackerDBContext.SaveChanges();
+
+
+        }
+    }
+}
